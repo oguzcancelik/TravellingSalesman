279fa0745670a9030912c0daf576349d4d5f71c8
Oguzcan Celik <oguzcancelik@marun.edu.tr>
2018-06-10 22:06:01 +0300

    Upgraded to 90% proximity

--- TravellingSalesman.py
+++ TravellingSalesman.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 def findClosest(i, data):
     dist = []
-    global visited, totalDist
+    global visited, totalDist, min, shiftVal
     visited.append(data[i][0])
     for j in range(len(data)):
         dist.append([j, round(
@@ -13,6 +13,9 @@ def findClosest(i, data):
     dist = sorted(dist, key=lambda row: row[1])
     data.pop(i)
     totalDist += dist[1][1]
+    if min > dist[1][1]:
+        min = dist[1][1]
+        shiftVal = len(visited)
     if dist[1][0] > i:
         return dist[1][0] - 1
     return dist[1][0]
@@ -22,9 +25,9 @@ def plot():
     global visited
     XX = []
     YY = []
-    for k in range(len(visited) - 1):
+    for k in range(len(visited)):
         XX.append(d2[visited[k]][1])
-        YY.append(d2[visited[k]][2])    
+        YY.append(d2[visited[k]][2])
     plt.plot(XX, YY)
     plt.show()
 
@@ -32,10 +35,13 @@ def plot():
 data = [[]]
 visited = []
 totalDist = 0
-input = '1.txt'
+input = input('Type name of the input file: ') + '.txt'
 with open(input) as file:
     lines = file.readlines()
 file.close()
+
+randNum = randint(0, len(lines) - 1)
+par = randNum
 
 for i in range(len(lines)):
     temp = [int(j) for j in lines[i].split()]
@@ -43,21 +49,27 @@ for i in range(len(lines)):
 data.pop()
 d2 = list(data)
 
-randNum = randint(0, len(lines) - 1)
-par = randNum
+min = round(math.sqrt(math.pow(data[0][1] - data[1][1], 2) + math.pow(data[0][2] - data[1][2], 2)))
+shiftVal = 0
 
 while len(data) != 1:
     par = findClosest(par, data)
 
 totalDist += round(math.sqrt(math.pow(d2[randNum][1] - data[0][1], 2) + math.pow(d2[randNum][2] - data[0][2], 2)))
-
 visited.append(data[0][0])
-visited.append(d2[randNum][0])
+
+for i in range(shiftVal):
+    visited.append(visited.pop(0))
+
+visited.append(visited[0])
 
 while 1:
     oldTotalDist = totalDist
-    for i in range(len(visited)):
-        for j in range(i + 2, len(visited) - 1):
+    for i in range(len(visited) - 1):
+        for j in range(0, len(visited) - 1):
+            if i == j or j == i + 1:
+                continue
+            # print('i,j: ', i, ', ', j)
             calc = round(math.sqrt(math.pow(d2[visited[i]][1] - d2[visited[i + 1]][1], 2) + math.pow(
                 d2[visited[i]][2] - d2[visited[i + 1]][2], 2))) + \
                    round(math.sqrt(math.pow(d2[visited[j - 1]][1] - d2[visited[j]][1], 2) + math.pow(
@@ -72,11 +84,17 @@ while 1:
                        d2[visited[j - 1]][2] - d2[visited[j + 1]][2], 2)))
             if calc > 0:
                 visited.insert(i + 1, visited[j])
-                del visited[j + 1]
                 totalDist -= calc
+                if i > j:
+                    del visited[j]
+                else:
+                    del visited[j + 1]
+                i = 0
+                break
     if totalDist == oldTotalDist:
         break
 
+# plot()
 visited.pop()
 visited.insert(0, totalDist)
 
@@ -86,3 +104,5 @@ file = open(output, 'w')
 for i in visited:
     file.write("%s\n" % i)
 file.close()
+
+print(totalDist)
